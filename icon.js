c911e15628656e71ac3773164972009d179168a8
Brendan Whiting <brendan.whiting@me.com>
2016-02-15 22:42:17 -0500

    made the button work

---
+++ icon.js
@@ -0,0 +1,29 @@
+var nav_showing = true;
+
+function setup_button() {
+    var button = document.getElementById("button");
+    var nav = document.getElementById("fancy-nav");
+    button.onclick = function() {
+        if (nav_showing) {
+            this.setAttribute("class", "button-off");
+            nav.setAttribute("class", "nav-off");
+            nav_showing = false;
+        } else {
+            this.setAttribute("class", "button-on");
+            nav.setAttribute("class", "nav-on");
+            nav_showing = true;
+        }
+        
+    };  
+
+    
+    
+}
+
+
+
+
+window.onload = function() {
+    // console.log(button);
+    setup_button();
+}; 
